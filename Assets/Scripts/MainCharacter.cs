fde1961e3bfae516106784db006b63c71cd8ef32
nazsarpasan <nazsarpasan@hotmail.com>
2023-12-16 19:25:55 +0300

    last

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
-using Cinemachine;
 
 public class MainCharacter : MonoBehaviour
 {
@@ -14,18 +12,11 @@ public class MainCharacter : MonoBehaviour
 
     Rigidbody2D rigitbody;
 
-    public Animator animator;
 
-    public CinemachineVirtualCamera cmv;
 
-    public float horizontal;
-
-    public GameObject karakter2;
 
     void Start()
     {
-        rigitbody = GetComponent<Rigidbody2D>();
-        
         
         /*if (typeof(MainCharacter).IsPublic)
         {
@@ -33,29 +24,12 @@ public class MainCharacter : MonoBehaviour
             IsMainCharacter = true;
         }*/
 
-        
+        rigitbody = GetComponent<Rigidbody2D>();
     }
-
-    /*private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.transform == karakter2)
-        {
-
-            if (collision.gameObject.GetComponent<MainCharacter>().IsMainCharacter == false)
-            {
-
-                IsMainCharacter = false;
-
-
-                collision.gameObject.GetComponent<MainCharacter>().IsMainCharacter = true;
-            }
-        }
-
-    }*/
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Karakter2")
+        if(collision.transform.tag == "Karakter2")
         {
             print("mahmut");
             if (collision.gameObject.GetComponent<MainCharacter>().IsMainCharacter == false)
@@ -65,77 +39,27 @@ public class MainCharacter : MonoBehaviour
 
 
                 collision.gameObject.GetComponent<MainCharacter>().IsMainCharacter = true;
-                
-                if (transform.tag == "Karakter1")
-                {
-                    animator.SetBool("IsIdle", true);
-                    animator.SetBool("IsWalking", false);
-                
-                }
-
-
-                Carpma();
             }
         }
-
-        
 
     }
 
 
-
+    
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
+        
         if (!IsMainCharacter)
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         }
         else
         {
-            
-            MoveCharacter();
-
-        }
-
-        
-    }
-
-    void MoveCharacter()
-
-    {
-        rigitbody.velocity = new Vector2(horizontal*3, 0);
-
-       /* if (Input.GetAxis("Horizontal") > 0)
-        {
-            transform.position += transform.right * Time.deltaTime;
-        }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            transform.position -= transform.right * Time.deltaTime;
-        }*/
-
-        
-        
-        if (horizontal!=0)
-        {
-            print("mahmut");
-            animator.SetBool("IsWalking" , true) ;
-            animator.SetBool("IsIdle", false);
-            
-        }
-        else
-        {
-            animator.SetBool("IsIdle",true);
-            animator.SetBool("IsWalking", false);
+            rigitbody.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         }
     }
 
-    void Carpma()
-    {
-        cmv.m_Follow = karakter2.transform;
-    }
+
 }
 
 
